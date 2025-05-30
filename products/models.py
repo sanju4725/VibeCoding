73f5fe1e7ba26ed4c57770e4e9eb3f6764aaf57f
sanju4725 <sanju4725@gmail.com>
2025-05-30 19:16:55 +0530

    Modified Product table

--- products/models.py
+++ products/models.py
@@ -8,5 +8,16 @@ class Product(models.Model):
     category = models.CharField(max_length=100)
     stock = models.PositiveIntegerField(default=0)
 
+    # New fields
+    discount = models.BooleanField(default=False)
+    discount_percentage = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.name
+
+    @property
+    def selling_price(self):
+        """Calculated price after discount"""
+        if self.discount and 0 <= self.discount_percentage <= 100:
+            return self.price * (1 - self.discount_percentage / 100)
+        return self.price
